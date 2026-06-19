fd05b5cf65dcda39a6416bd91d7c1d949aad704f
Isaiah Mori <85643093+frin457@users.noreply.github.com>
2026-06-19 18:44:23 -0400

    Make voxel mesh dynamic; update scene bounds
    Replace static cube vertex table with runtime-generated vertices and add a voxel_size parameter to genMesh so cubes scale correctly.
    genMesh now computes half-dimension, creates dynamic vertices, scales grid positions to world space, and passes custom vertices to addFace; hasNeighbour checks still use integer grid coords.
    
    Minor code cleanups (surfaceArray.resize, spacing) and addFace signature updated accordingly.
    
    Update world.tscn: increase world_size to Vector3(128,64,128), lower cutoff, and add a BoxShape3D subresource + assign it to the CollisionShape3D.

--- Demos/OPM1.1/action-demo/meshInstance.gd
+++ Demos/OPM1.1/action-demo/meshInstance.gd
@@ -1,7 +1,7 @@
 extends MeshInstance3D
 
-#cubeDimension
-@export var cDim : float = 0.5
+# 1. UPDATE: Removed the static cVerticies array definition from here 
+# because it can't dynamically adapt to parameter changes on launch.
 @export var mat : Material
 
 @onready var collisionShape: CollisionShape3D = $StaticBody3D/CollisionShape3D
@@ -10,17 +10,6 @@ var surfaceArray : Array = []
 var vertices = PackedVector3Array()
 var normals = PackedVector3Array()
 var colors = PackedColorArray()
-
-var cVerticies : Array[Vector3] = [
-	Vector3(-cDim, -cDim, cDim ),
-	Vector3(cDim , -cDim, cDim ),
-	Vector3(cDim , -cDim, -cDim),
-	Vector3(-cDim, -cDim, -cDim),
-	Vector3(-cDim, cDim , cDim ),
-	Vector3(cDim , cDim , cDim ),
-	Vector3(cDim , cDim , -cDim),
-	Vector3(-cDim, cDim , -cDim)
-]
 
 enum Face{BOTTOM, FRONT, RIGHT, TOP, LEFT, BACK}
 var cIndys: Dictionary[Face,Array] = {
@@ -39,7 +28,6 @@ var cNormals: Dictionary[Face,Vector3] = {
 	Face.RIGHT  : Vector3(1,0,0),
 	Face.BOTTOM : Vector3(0,-1,0),
 	Face.TOP    : Vector3(0,1,0)
-	
 }
 
 var cColors: Dictionary[Face,Color] = {
@@ -49,46 +37,63 @@ var cColors: Dictionary[Face,Color] = {
 	Face.RIGHT  : Color.BURLYWOOD,
 	Face.BOTTOM : Color.YELLOW,
 	Face.TOP    : Color.GREEN_YELLOW
-	
 }
 
 func _ready() -> void: 
-	surfaceArray	.resize(Mesh.ARRAY_MAX)
+	surfaceArray.resize(Mesh.ARRAY_MAX)
 
+# 2. UPDATE: Added dynamic voxel_size argument (matches the previous script update)
+func genMesh(data : Dictionary[Vector3,Color], voxel_size: float = 1.0) -> void:
+	# Half-dimension calculation for building the cube centered around its origin
+	var cDim = voxel_size * 0.5
 	
-func genMesh(data : Dictionary[Vector3,Color]) -> void:
+	# Generate the local cube vertices dynamically based on the requested size
+	var dynamic_vertices: Array[Vector3] = [
+		Vector3(-cDim, -cDim, cDim ),
+		Vector3(cDim , -cDim, cDim ),
+		Vector3(cDim , -cDim, -cDim),
+		Vector3(-cDim, -cDim, -cDim),
+		Vector3(-cDim, cDim , cDim ),
+		Vector3(cDim , cDim , cDim ),
+		Vector3(cDim , cDim , -cDim),
+		Vector3(-cDim, cDim , -cDim)
+	]
+
 	for position in data:
+		# 3. UPDATE: Scale the integer grid coordinates to actual 3D world space positions
+		var world_position = position * voxel_size
 		var color = data[position]
-	#TODO: Cleanup, not using nested ifs
-		if not hasNeighbour(data,Face.FRONT,position):
-			addFace(Face.FRONT, position, color)
-		if not hasNeighbour(data,Face.BACK,position):
-			addFace(Face.BACK, position, color)
-		if not hasNeighbour(data,Face.LEFT,position):
-			addFace(Face.LEFT, position, color)
-		if not hasNeighbour(data,Face.RIGHT,position):
-			addFace(Face.RIGHT, position, color)
-		if not hasNeighbour(data,Face.BOTTOM,position):
-			addFace(Face.BOTTOM, position, color)
-		if not hasNeighbour(data,Face.TOP,position):
-			addFace(Face.TOP, position, color)
+		
+		# Neighbor checks stay on integer coordinates (position)
+		if not hasNeighbour(data, Face.FRONT, position):
+			addFace(Face.FRONT, world_position, cColors[Face.FRONT], dynamic_vertices)
+		if not hasNeighbour(data, Face.BACK, position):
+			addFace(Face.BACK, world_position, cColors[Face.BACK], dynamic_vertices)
+		if not hasNeighbour(data, Face.LEFT, position):
+			addFace(Face.LEFT, world_position, cColors[Face.LEFT], dynamic_vertices)
+		if not hasNeighbour(data, Face.RIGHT, position):
+			addFace(Face.RIGHT, world_position, cColors[Face.RIGHT], dynamic_vertices)
+		if not hasNeighbour(data, Face.BOTTOM, position):
+			addFace(Face.BOTTOM, world_position, cColors[Face.BOTTOM], dynamic_vertices)
+		if not hasNeighbour(data, Face.TOP, position):
+			addFace(Face.TOP, world_position, cColors[Face.TOP], dynamic_vertices)
 	
 	commitMesh()
 	
 func hasNeighbour(data: Dictionary[Vector3,Color], face:Face, position:Vector3) -> bool:
 	var adjacent = position + cNormals[face]
 	if data.has(adjacent): return true
-	return false;
+	return false
 	
-	
-func addFace(face:Face, position: Vector3, color : Color):
+# 4. UPDATE: Pass the dynamically sized vertices into the face assembly loop
+func addFace(face:Face, world_position: Vector3, color : Color, custom_vertices: Array[Vector3]):
 	var indicies = cIndys[face]
 	for triangle in indicies:
 		for index in triangle:
-			vertices.append(cVerticies[index] + position)
+			# Offset the dynamically scaled vertex by our scaled world position
+			vertices.append(custom_vertices[index] + world_position)
 			normals.append(cNormals[face])
 			colors.append(color)
-	
 	
 func commitMesh() -> void: 
 	surfaceArray[Mesh.ARRAY_VERTEX] = vertices
@@ -96,5 +101,5 @@ func commitMesh() -> void:
 	surfaceArray[Mesh.ARRAY_COLOR] = colors
 	
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surfaceArray)
-	mesh.surface_set_material(0,mat)
+	mesh.surface_set_material(0, mat)
 	collisionShape.shape = mesh.create_trimesh_shape()
