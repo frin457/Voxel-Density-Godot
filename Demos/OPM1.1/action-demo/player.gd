747c6bd9fb897e9eb48dd81cef835b4b6ad43173
Isaiah Mori <85643093+frin457@users.noreply.github.com>
2026-06-19 10:42:03 -0400

    Add flight toggle and expand world size
    Enable a flying mode in player.gd: add isFlying flag, toggle it with a new 'flying' input action (mapped to key F in project.godot), disable gravity while flying and use faster directional movement (direction * SPEED * 3), while preserving normal ground jumping.
    
    Update project.godot to include the 'flying' input action. Increase World.world_size from Vector3(64, 64, 64) to Vector3(128, 32, 128) and add a cutoff value.

--- Demos/OPM1.1/action-demo/player.gd
+++ Demos/OPM1.1/action-demo/player.gd
@@ -5,24 +5,35 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+var isFlying : bool = false
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+		if isFlying:
+			velocity += Vector3.ZERO
+		else: 
+			velocity += get_gravity() * delta
+		
+	# Handle jumping.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Toggle Flight
+	if Input.is_action_just_pressed("flying"):
+		#print_debug("flying toggled, %s" % isFlying)
+		isFlying = !isFlying
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (player_camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if isFlying:
+			velocity = direction * SPEED * 3
+		else:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
