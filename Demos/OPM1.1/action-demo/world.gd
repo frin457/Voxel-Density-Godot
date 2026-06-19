057ddb5552eb92e38788027071b529af92c3f95d
Isaiah Mori <85643093+frin457@users.noreply.github.com>
2026-06-19 10:00:46 -0400

    Use MultiMesh for procedural cubes
    Stop instantiating individual cube nodes and populate a MultiMeshInstance3D from generated block positions instead.
    
    Added onready reference to the MultiMeshInstance3D, collect cube positions into a data array, set multimesh.instance_count and per-instance transforms.
    
    Appends a custom Performance monitor "game/cubes" and basic generation timing/debug output.
    This reduces node overhead during world generation and centralizes instance handling for better performance.

--- Demos/OPM1.1/action-demo/world.gd
+++ Demos/OPM1.1/action-demo/world.gd
@@ -6,22 +6,38 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 @onready var default_cube : CSGBox3D = $DefaultCube
 @onready var default_camera: Camera3D = $DefaultCamera
+@onready var mesh_instance : MultiMeshInstance3D = $MultiMeshInstance3D
 
+#Custom data
+var cubes: int = 0	
+var data: Array[Vector3] = []
 func _ready() -> void:
+	#set size of the multizmesh buffer
+	Performance.add_custom_monitor("game/cubes", func(): return cubes)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	var random_gen = FastNoiseLite.new()	
 	
-	var random_gen = FastNoiseLite.new()
+	var startTime = Time.get_ticks_usec()
+	 
 	#var random_gen = RandomNumberGenerator.new()
 	for x in range(world_size.x):
 		for z in range(world_size.z):
 			for y in range(2, 2 + world_size.y):
-				var random_num = random_gen.get_noise_3d(x,y,z)
-				#var random_num = random_gen.randf()
+				var random_num = random_gen.get_noise_3d(x,y,z) 
 				if random_num > cutoff:
-					var new_cube = default_cube.duplicate()
-					new_cube.position = Vector3(x,y,z)
-					add_child(new_cube)
+					data.append(Vector3(x,y,z))
 					
+					
+	#Calc generation statistics
+	var endTime = Time.get_ticks_usec()
+	var genTime = (endTime - startTime) / 100000
+	print_debug("Blocks generated %s\n Gen Time: %s seconds" % [cubes,genTime])
+	
+	#Set Multimesh sizing
+	mesh_instance.multimesh.instance_count = data.size()
+	for i in range(mesh_instance.multimesh.instance_count):
+		mesh_instance.multimesh.set_instance_transform(i, Transform3D(Basis(), data[i]))
+	
 	default_camera.position = Vector3(world_size.x/2,world_size.y* 0.75,world_size.z)
 	default_camera.rotation = Vector3(-45,-25,0)
 	
