50d8380ef5f518871a5fb2b144cb6ed2552fd975
Isaiah Mori <85643093+frin457@users.noreply.github.com>
2026-06-19 10:20:02 -0400

    Add per-instance coloring to multimesh
    Export a colors array and assign a random color to each MultiMesh instance.
    The script now increments the cube counter when generating blocks and calls set_instance_color during mesh population.
    The scene file was updated to provide a default color palette on the World node and the material was switched to use vertex colors as albedo.
    Also commented out the default camera positioning and applied minor formatting/cleanup.

--- Demos/OPM1.1/action-demo/world.gd
+++ Demos/OPM1.1/action-demo/world.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var world_size: Vector3 =  Vector3(16,16,16)
+@export var colors : Array[Color]
 @export_range(-1,1) var cutoff: float = 0.5
 
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +10,10 @@ extends Node3D
 @onready var mesh_instance : MultiMeshInstance3D = $MultiMeshInstance3D
 
 #Custom data
-var cubes: int = 0	
+var cubes: int = 0
 var data: Array[Vector3] = []
 func _ready() -> void:
-	#set size of the multizmesh buffer
+	
 	Performance.add_custom_monitor("game/cubes", func(): return cubes)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	var random_gen = FastNoiseLite.new()	
@@ -26,20 +27,22 @@ func _ready() -> void:
 				var random_num = random_gen.get_noise_3d(x,y,z) 
 				if random_num > cutoff:
 					data.append(Vector3(x,y,z))
-					
+					cubes += 1
 					
 	#Calc generation statistics
 	var endTime = Time.get_ticks_usec()
 	var genTime = (endTime - startTime) / 100000
 	print_debug("Blocks generated %s\n Gen Time: %s seconds" % [cubes,genTime])
 	
-	#Set Multimesh sizing
+	#Set size of the multimesh buffer
 	mesh_instance.multimesh.instance_count = data.size()
 	for i in range(mesh_instance.multimesh.instance_count):
 		mesh_instance.multimesh.set_instance_transform(i, Transform3D(Basis(), data[i]))
+		mesh_instance.multimesh.set_instance_color(i, colors[randf() * colors.size()])
 	
-	default_camera.position = Vector3(world_size.x/2,world_size.y* 0.75,world_size.z)
-	default_camera.rotation = Vector3(-45,-25,0)
+	
+	#default_camera.position = Vector3(world_size.x/2,world_size.y* 0.75,world_size.z)
+	#default_camera.rotation = Vector3(-45,-25,0)
 	
 	remove_child(default_cube) #Remove default cube
 	
